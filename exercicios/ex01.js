cae4278da5fdbc8f72ea3640c2ff008bc67329a3
Tifany Paola <tifanytp419@gmail.com>
2024-09-26 13:11:17 -0300

    ex01

---
+++ exercicios/ex01.js
@@ -0,0 +1,5 @@
+// Elaborar um programa que leia um número. Calcule e informe os seus vizinhos, ou seja, o número anterior e posterior.
+num = Number(prompt("Digite um número: "));
+
+alert(`O sucessor de ${num} é ${num + 1} e o anterior é ${num - 1}`);
+
