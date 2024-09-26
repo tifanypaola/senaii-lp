203fa32b4d74fde979dbb2917c18fdb78229ca9f
Tifany Paola <tifanytp419@gmail.com>
2024-09-26 13:12:31 -0300

    ex04

---
+++ exercicios/ex04.js
@@ -0,0 +1,7 @@
+//Elaborar um programa que leia 2 notas de um aluno em uma disciplina.
+//Calcule e informe a média das notas.
+
+const nota1 = parseFloat(prompt("Digite a primeira nota: "));
+const nota2 = parseFloat(prompt("Digite a segunda nota: "));
+
+alert(`Média ${( nota1 + nota2) / 2}`);
